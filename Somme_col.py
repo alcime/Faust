0c2f9f8a3c17a18812d2e69dee67ee5825c2d68a
root <root@localhost.localdomain>
2019-11-11 12:57:15 +0100

    Ca fontionne presque, sauf pour la derniere colonne

--- Somme_col.py
+++ Somme_col.py
@@ -2,32 +2,67 @@
 
 import pandas as pd
 
-infile = input("Nom du fichier d'entree :")
+# Demande le nom du fichier
+input_file_name = input("Nom du fichier d'entree :")
 print ('\n')
-if infile == '':
-   infile = 'tsv_simple.tsv'
-outfile = 'Promesse_somme.tsv'
+if input_file_name == '':
+   input_file_name = 'tsv_simple.tsv'
+
+# Parametres
+ouput_file_name = 'Promesse_somme.tsv'
 nb_lignes_a_sauter = 0 #23277
-
 i_col_titre = 0 #1
-i_first_col = 0 #4
+i_first_col = 1 #4
 i_last_col = 2 #49
-               
-fout = open(outfile, "w+")
-fin = pd.read_csv(infile, sep = '\t', header = 0, skiprows = nb_lignes_a_sauter )
+output_to_console = True
+output_to_file = True
 
-               
-for i_row in range(1, fin.shape[0] - 1):
-    print('i_row = ' + str(i_row) + '\n')
-    if fin.iloc[i_row][i_col_titre] == fin.iloc[i_row + 1][i_col_titre] :
-        for i_col in range(i_first_col, i_last_col):
-            print('i_row = ' + str(i_row) + '\n')
-            print ('Ancienne valeur de la col#' + str(i_col+1) + ' : ' + str(fin.iloc[i_col + 1]) )
-            fin.iloc[i_col + 1] += fin.iloc[i_col]
-            print ('Nouvelle valeur de la col#' + str(i_col+1) + ' : ' + str(fin.iloc[i_col + 1]) )
-    else:
-        print('pas de modif pour cette ligne')
-        line = pd.DataFrame.to_csv(fin.iloc[:i_row], sep = '\t')
-        fout.write(line)
-    
-fout.close()
+# Read input file contents to df
+df = pd.read_csv(input_file_name, sep = '\t', header = 0, skiprows = nb_lignes_a_sauter )
+row_count = df.shape[0]
+col_count = df.shape[1]
+
+
+
+# Print df
+print('Data found in ' + input_file_name)
+for i_row in range(0, row_count):
+   for i_col in range(0, col_count) :
+     cell_contents = str(df.iloc[i_row][i_col])
+     if (i_col >= i_first_col) & (i_col <= i_last_col) :
+        print(' *' + cell_contents, end = '')
+     else :
+        print('  ' + cell_contents, end = '')
+   print('')
+print('\n')
+   
+ 
+
+# Sommation des colonnes
+output_file = open(ouput_file_name, "w")
+for i_row in range(0, row_count - 1):
+   print('i_row = ' + str(i_row) + '\n')
+
+   if i_row == row_count - 1 :
+      if output_to_console :
+         print('pas de modif pour cette ligne \n')
+      if output_to_file :
+         line = pd.DataFrame.to_csv(df.iloc[i_row:], sep = '\t')
+         output_file.write(line)
+
+   elif df.iloc[i_row, i_col_titre] == df.iloc[i_row + 1, i_col_titre] :
+      for i_col in range(i_first_col, i_last_col) :
+         if output_to_console :
+            print ('Ancienne valeur de la col#' + str(i_col+1) + ' : \n' + str(df.iloc[i_row+1][i_col + 1]) )
+         df.iloc[i_row + 1, i_col] += df.iloc[i_row, i_col]
+         if output_to_console :
+            print ('Nouvelle valeur de la col#' + str(i_col+1) + ' : \n' + str(df.iloc[i_row + 1][i_col + 1]) )
+   
+   else :
+      if output_to_console :
+         print('pas de modif pour cette ligne \n')
+      if output_to_file :
+         line = pd.DataFrame.to_csv(df.iloc[i_row:], sep = '\t')
+         output_file.write(line)
+         
+output_file.close()
